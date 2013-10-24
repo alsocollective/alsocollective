6fbeebc437311bf7c97be3a2b16f710123e7178a
symonoliver <symon@alsocollective.com>
2013-10-24 16:09:33 -0400

    fixed analytics

--- static/js/ganalytics.js
+++ static/js/ganalytics.js
@@ -9,5 +9,3 @@ _gaq.push(['_trackPageview']);
 	ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
-
-var pageTracker = _gat._getTracker("UA-37086718-1");
